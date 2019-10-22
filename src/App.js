7c797a3c1fd541edba5334386c95c6dd6913cffc
ida.pousse <idapousse@yahoo.fr>
2019-10-22 12:34:51 +0200

    Buil parent component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CommentDetail from './components/CommentDetail';
-import ApprovalCard from './components/ApprovalCard';
 
 
 const App = () => {
@@ -8,28 +7,26 @@ const App = () => {
 
     <div className="ui container comments">
       <CommentDetail 
-        author=""
-        avatar=""
-        timeAgo=""
-        content=""
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        comment="Nice blog post!"
+        avatar={faker.image.avatar()}
       />
 
       <CommentDetail 
-        author=""
-        avatar=""
-        timeAgo=""
-        content=""
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        comment="Very well written, the 2nd part of the blog really resonnated with me."
+        avatar={faker.image.avatar()}
       />
 
       <CommentDetail 
-        author=""
-        avatar=""
-        timeAgo=""
-        content=""
+        author="Jane"
+        timeAgo="Today at 5:00PM"
+        comment="Admire your prose as usual, even if I don't necessarily agree with you on this particular subject!"
+        avatar={faker.image.avatar()}
       />
     </div>
-
-
 
    );
 }

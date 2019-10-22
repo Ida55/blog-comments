fcc2a984b8954d352ca383bad0e8eadfd9c41273
ida.pousse <idapousse@yahoo.fr>
2019-10-22 14:11:13 +0200

    Component Reusability

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import CommentDetail from './components/CommentDetail';
+import ApprovalDetail from './components/ApprovalDetail';
 import faker from 'faker';
 
 
 const App = () => {
   return ( 
-
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        comment="Nice blog post!"
-        avatar={faker.image.avatar()}
-      />
 
-      <CommentDetail 
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        comment="Very well written, the 2nd part of the blog really resonnated with me."
-        avatar={faker.image.avatar()}
-      />
-
-      <CommentDetail 
-        author="Jane"
-        timeAgo="Today at 5:00PM"
-        comment="Admire your prose as usual, even if I don't necessarily agree with you on this particular subject!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalDetail>
+        <CommentDetail 
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          comment="Nice blog post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalDetail>
+    
+      <ApprovalDetail>
+        <CommentDetail 
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          comment="Very well written, the 2nd part of the blog really resonnated with me."
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalDetail>
+      
+      <ApprovalDetail>
+        <CommentDetail 
+          author="Jane"
+          timeAgo="Today at 5:00PM"
+          comment="Admire your prose as usual, even if I don't necessarily agree with you on this particular subject!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalDetail>
     </div>
-
    );
 }
  
